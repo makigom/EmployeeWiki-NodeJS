7f2e30da0da30afd860cc7b5c5fbd44c720a5a24
makigom <makigom@gmai.com>
2016-06-17 19:25:09 -0300

    Retoques Finales

--- fixtures/employees.js
+++ fixtures/employees.js
@@ -1,5 +1,7 @@
 exports.Employees = [
 	{ name:'Macarena', lastName:'Gomez', email:'makigom@gmail.com', password:'holamundo'},
 	{ name:'Raul', lastName:'Rios', email:'rrios@yahoo.com', password:'raulrios' },
-	{ name:'Marta', lastName:'Suarez', email:'msuarez@yahoo.com', password:'martasuarez'}
+	{ name:'Marta', lastName:'Suarez', email:'msuarez@yahoo.com', password:'martasuarez'},
+	{ name:'Carlos', lastName:'Gomez', email:'cgomez@outlook.con', password:'carlosgomez'},
+	{ name:'Marcela', lastName:'Giovanelli', email:'mgiovanelli@gmail.com', password:'mgiovanelli'}
 ] 
